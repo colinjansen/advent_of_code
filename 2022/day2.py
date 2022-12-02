fbd40d795179e84e4344a54c1ac9544f97a5a966
Colin Jansen <cjansen@replicationconsulting.ca>
2022-12-02 10:18:10 -0700

    better day 2

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,44 +1,18 @@
+mapping = { 'A': 0, 'B': -1, 'C': 1, 'X': 1, 'Y': 2, 'Z': 3 }
+
 with open("_input/day2.txt", encoding='utf8') as f:
-    lines = f.read().splitlines() 
+    lines =  f.read().splitlines()
+    input = map(lambda s: s.split(' '), lines)
 
-p1 = { 'A': 0, 'B': -1, 'C': 1, 'X': 1, 'Y': 2, 'Z': 3 }
-p2 = { 'A': 1, 'B': 2, 'C': 3, 'X': 0, 'Y': 3, 'Z': 6 }
+sets = map(lambda x: (mapping[x[0]], mapping[x[1]]), input)
 
-def part1():
-    acc = 0
-    for game in lines:
-        g = game.split(' ')
-        a = p1[g[0]]
-        x = p1[g[1]]
-        acc += x + (3 * (x + a) % 9)
-    return acc
+def doEet(sets):
+    p1 = 0
+    p2 = 0
+    for a, x in sets:
+        p1 += x + (3 * (x + a) % 9)
+        p2 += ((x - a + 1) % 3) + 1 + (3 * (x - 1))
+    return p1, p2
 
-def part2():
-    acc = 0
-    for game in lines:
-        g = game.split(' ')
-        a = p1[g[0]]
-        x = p1[g[1]]
- 
-        acc += x + (3 * (x + a ) % 9)
-
-        # x + ( (x + 9) % 9 )                                        a x
-
-        # if (g[0] == 'A' and g[1] == 'X'): acc += (3 + 0)  # lose   1 0 = 3
-        # if (g[0] == 'A' and g[1] == 'Y'): acc += (1 + 3)  # draw   1 3 = 1
-        # if (g[0] == 'A' and g[1] == 'Z'): acc += (2 + 6)  # win    1 6 = 2
-
-        # if (g[0] == 'B' and g[1] == 'X'): acc += (1 + 0)           2 0 = 1
-        # if (g[0] == 'B' and g[1] == 'Y'): acc += (2 + 3)           2 3 = 2
-        # if (g[0] == 'B' and g[1] == 'Z'): acc += (3 + 6)           2 6 = 3
-
-        # if (g[0] == 'C' and g[1] == 'X'): acc += (2 + 0)           3 0 = 2
-        # if (g[0] == 'C' and g[1] == 'Y'): acc += (3 + 3)           3 3 = 3
-        # if (g[0] == 'C' and g[1] == 'Z'): acc += (1 + 6)           3 6 = 1
-
-    return acc
-
-print( (6 + 9) % 9 )
-
-print(part1()) # 15632
-print(part2()) # 14416
+p1, p2 = doEet(sets)
+print(f'part 1 is {p1}, part 2 is {p2}')
