e9bdf648995dd2fcd5321cb8421d8a976d8800f1
Colin Jansen <cjansen@replicationconsulting.ca>
2022-12-01 23:45:24 -0700

    sloppy day 2 solution

--- 2022/day2.py
+++ 2022/day2.py
@@ -1,16 +1,35 @@
-with open("_input/day1.txt", encoding='utf8') as f:
+with open("_input/day2.txt", encoding='utf8') as f:
     lines = f.read().splitlines() 
 
-acc = 0
-nums = []
-for num in lines:
-    if (num == ''):
-        nums.append(acc)
-        acc = 0
-        continue
-    acc += int(num)
-if (acc > 0):
-    nums.append(acc)
-    
-print(f'part 1: {max(nums)}')
-print(f'part 2: {sum(sorted(nums)[-3:])}')
+def part1():
+    acc = 0
+    for game in lines:
+        g = game.split(' ')
+        if (g[0] == 'A' and g[1] == 'X'): acc += (1 + 3)  # r v r
+        if (g[0] == 'A' and g[1] == 'Y'): acc += (2 + 6)  # r v
+        if (g[0] == 'A' and g[1] == 'Z'): acc += (3 + 0)
+        if (g[0] == 'B' and g[1] == 'X'): acc += (1 + 0)
+        if (g[0] == 'B' and g[1] == 'Y'): acc += (2 + 3)
+        if (g[0] == 'B' and g[1] == 'Z'): acc += (3 + 6)
+        if (g[0] == 'C' and g[1] == 'X'): acc += (1 + 6)
+        if (g[0] == 'C' and g[1] == 'Y'): acc += (2 + 0)
+        if (g[0] == 'C' and g[1] == 'Z'): acc += (3 + 3)
+    return acc
+
+def part2():
+    acc = 0
+    for game in lines:
+        g = game.split(' ')
+        if (g[0] == 'A' and g[1] == 'X'): acc += (3 + 0)  # lose
+        if (g[0] == 'A' and g[1] == 'Y'): acc += (1 + 3)  # draw
+        if (g[0] == 'A' and g[1] == 'Z'): acc += (2 + 6)  # win
+        if (g[0] == 'B' and g[1] == 'X'): acc += (1 + 0)
+        if (g[0] == 'B' and g[1] == 'Y'): acc += (2 + 3)
+        if (g[0] == 'B' and g[1] == 'Z'): acc += (3 + 6)
+        if (g[0] == 'C' and g[1] == 'X'): acc += (2 + 0)
+        if (g[0] == 'C' and g[1] == 'Y'): acc += (3 + 3)
+        if (g[0] == 'C' and g[1] == 'Z'): acc += (1 + 6)
+    return acc
+
+print(part1())
+print(part2())
