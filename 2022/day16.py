9975c5b8ca109422b477c0608bbd833f55ca3432
Colin Jansen <cjansen@replicationconsulting.ca>
2022-12-18 20:34:29 -0700

    finally finished day 17 just in time for day 18

--- 2022/day16.py
+++ 2022/day16.py
@@ -1,2 +1,72 @@
-with open("_input/day16.txt.test", encoding='utf8') as f:
-    input = f.read().splitlines()
+import re
+from collections import deque
+
+with open("_input/day16.txt", encoding='utf8') as f:
+    lines = f.read().splitlines()
+
+
+valves = {}
+tunnels = {}
+dists = {}
+cache = {}
+nonempty = []
+
+for line in lines:
+    g = re.match(
+        'Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? ([\w, ]+)', line).groups()
+    valves[g[0]] = int(g[1])
+    tunnels[g[0]] = g[2].split(', ')
+
+#
+# get distances to neighbouring valves
+#
+for valve in valves:
+    if valve != 'AA' and not valves[valve]:
+        continue
+    if valve != 'AA':
+        nonempty.append(valve)
+    dists[valve] = {valve: 0, 'AA': 0}
+    visited = {valve}
+    queue = deque([(0, valve)])
+    while queue:
+        distance, position = queue.popleft()
+        for neighbour in tunnels[position]:
+            if neighbour in visited:
+                continue
+            visited.add(neighbour)
+            if valves[neighbour]:
+                dists[valve][neighbour] = distance + 1
+            queue.append((distance + 1, neighbour))
+    del dists[valve][valve]
+    if valve != 'AA':
+        del dists[valve]['AA']
+
+indices = {}
+for i, el in enumerate(nonempty):
+    indices[el] = i
+
+
+def search(time, valve, mask):
+    if (time, valve, mask) in cache:
+        return cache[(time, valve, mask)]
+    max_flow = 0
+    for neighbour in dists[valve]:
+        bit = 1 << indices[neighbour]
+        if mask & bit:
+            continue
+        remaining_time = time - dists[valve][neighbour] - 1
+        if remaining_time <= 0:
+            continue
+        max_flow = max(max_flow, search(remaining_time, neighbour, mask | bit) + (valves[neighbour] * remaining_time))
+    cache[(time, valve, mask)] = max_flow
+    return max_flow
+
+part1 = search(30, 'AA', 0)
+
+b = (1 << len(nonempty)) - 1
+
+m = 0
+for i in range((b + 1) // 2):
+    m = max(m, search(26, 'AA', i) + search(26, 'AA', b ^ i))
+
+print(part1, m)
--- 2022/day17.py
+++ 2022/day17.py
@@ -1,99 +1,148 @@
+import time
+
 with open('_input/day17.txt') as f:
     input = f.read()
 
 
-wall = 7
 input_length = len(input)
-top = -1
-input_p = 0
-shape_p = 0
 shapes = [
-    [(0,0), (1,0), (2,0), (3,0)], # horizontal line
-    [(0,1), (1,0), (1,1), (1,2), (2,1)], # plus shape
-    [(0,0), (1,0), (2,0), (2,1), (2,2)], # L shape
-    [(0,0), (0,1), (0,2), (0,3)], # vertical line
-    [(0,0), (0,1), (1,1), (1,0)], # square
+    [60],             # horizontal line
+    [16, 56, 16],     # plus shape
+    [56, 8, 8],       # L shape
+    [32, 32, 32, 32],  # vertical line
+    [48, 48],         # square
 ]
+grid = {
+    2: 0,
+    1: 0,
+    0: 0,
+    -1: 254
+}
+highest_rock = -1
+shape_p = 0
+input_p = 0
 
-grid = {}
-
-def draw_grid(s=None):
-    g = [[' '] * wall for _ in range(get_highest_rock() + 6)]
-    for p in grid:
-        g[p[1]][p[0]] = '#'
-    if s != None:
-        for p in s.pos():
-            g[p[1]][p[0]] = '-'
-    for i in range(len(g)-1, -1, -1):
-        line = ''.join(g[i])
-        print(f'[{line}]')
 
 def get_push():
     """
     get the direction to move the shape
     """
-    global input_p
+    global input_p, shape_p
     c = input[input_p]
     input_p = (input_p + 1) % input_length
     return 1 if c == '>' else -1
 
-def get_shape():
+
+def get_shape(insert_at: int) -> dict:
     """
-    get the next shape to use
+    get the next direction to 'push' in
     """
     global shape_p
-    s = [*shapes[shape_p]]
+    s = {insert_at + i: v for i, v in enumerate([*shapes[shape_p]])}
     shape_p = (shape_p + 1) % len(shapes)
-    return Shape(s, 2, top + 4)
+    return s
 
-def check_collions(points: dict):
-    for k in points.keys():
-        if k[0] < 0 or k[0] >= wall or k in grid or k[1] < 0: return True
-    return False
 
-def add_shape_to_grid(points, t):
-    global top 
-    global grid
-    top = max(top, t)
-    grid.update(points)
+def add_shape_to_grid(grid: dict[int, int], shape: dict[int, int], top: int) -> tuple[int, dict[int, int]]:
+    """
+    adds the shape to the 'rocks' of the grid
+    """
+    for k in shape:
+        grid[k] = grid[k] | shape[k]
+    return (max(top, list(shape)[-1:][0]), grid)
 
-class Shape:
 
-    def __init__(self, points, x = 0, y = 0):
-        self.points = points
-        self.x = x
-        self.y = y
-        self.w = max(points, key=lambda x: x[0])[0] + 1
-        self.h = max(points, key=lambda x: x[1])[1] + 1
+def draw_grid(grid: dict[int, int], shape: dict[int, int] = None, tail: int = None):
+    """
+    draw a representation of the current state of the graph with
+    an optional shape
+    """
+    g = dict(sorted(grid.items(), reverse=True))
+    if shape != None:
+        for k in shape:
+            g[k] = (g[k] if k in g else 0) | shape[k]
+    for k in g:
+        if tail > 0:
+            tail -= 1
+        print(f'{k+1: >8} : 1{bin(g[k] + 1)[2:]:0>8}')
+        if tail == 0:
+            return
 
-    def __repr__(self) -> str:
-        return f'{self.x} {self.y} {self.w} {self.h} {self.pos()}'
 
-    def move(self, x_d, y_d):
-        p_x = self.pos(x_d, 0)
-        if check_collions(p_x) == False:
-            self.x += x_d
+def move(grid: dict[int, int], shape: dict[int, int]) -> dict[int, int]:
+    """
+    try to move the shape from side to side
+    """
+    shift = get_push()
+    if shift == -1:
+        s = {k: shape[k] << 1 for k in shape}
+        for k in shape:
+            if s[k] & (256 | grid[k]):
+                return shape
+        return s
+    if shift == 1:
+        s = {k: shape[k] >> 1 for k in shape}
+        for k in shape:
+            if s[k] & (1 | grid[k]):
+                return shape
+        return s
+    return shape
 
-        p_y = self.pos(0, y_d)
-        if check_collions(p_y) == False:
-            self.y += y_d
-            return True
-        
-        p_f = self.pos(0, 0)
-        add_shape_to_grid(p_f, self.y + self.h - 1)
-        return False
 
-    def pos(self, x=0, y=0):
-        return {(p[0] + self.x + x, p[1] + self.y + y): 0 for p in self.points}
+def drop(grid: dict[int, int], shape: dict[int, int]) -> tuple[bool, dict[int, int]]:
+    """
+    try to drop the shape down one row
+    """
+    dropped = {k - 1: shape[k] for k in shape}
+    for k in dropped:
+        if dropped[k] & grid[k] > 0:
+            return False, shape
+    return True, dropped
 
-def get_top_at_iteration(i):                                                    
-    for i in range(0, i):
-        s = get_shape()
-        if i % 100_000 == 0:
-            print(f"\rcycle: {i}", end='')
-        while s.move(get_push(), -1):
-            pass
-    print(f"\n{top + 1}")
 
-#get_top_at_iteration(2022)
-get_top_at_iteration(1_000_000_000_000)
+def run_simulation(t: int) -> int:
+    global grid, highest_rock, shape_p, input_p
+    """
+    for each 1730 iterations we gain 2644 blocks of height except
+    for the first iteration that seems to be 15 higher than the
+    others
+    """
+
+    # set out state to the starting positions
+    grid = { 2: 0, 1: 0, 0: 0, -1: 254 }
+    highest_rock = -1
+    shape_p = 0
+    input_p = 0
+
+    iterations_per_loop = 1730
+    height_gain_per_iteration = 2644
+    loops = t // iterations_per_loop
+    starting_height = (height_gain_per_iteration * loops) + 15
+    remaining_iterations = t - (iterations_per_loop * loops)
+
+    for _ in range(iterations_per_loop + remaining_iterations):
+
+        shape = get_shape(highest_rock + 4)
+        for k in shape:
+            grid[k] = 0
+
+        while True:
+            #
+            # push the shape
+            #
+            shape = move(grid, shape)
+            #
+            # drop the shape
+            #
+            dropped, shape = drop(grid, shape)
+            if (dropped == False):
+                break
+
+        highest_rock, grid = add_shape_to_grid(grid, shape, highest_rock)
+
+    return starting_height + (highest_rock - 2659) + 1
+
+
+start = time.time()
+print(f'part 1: {run_simulation(2022)} @ {round(1000 * (time.time() - start), 3)}ms')
+print(f'part 2: {run_simulation(1_000_000_000_000)} @ {round(1000 * (time.time() - start), 3)}ms')
