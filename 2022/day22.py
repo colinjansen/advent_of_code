cf0c7c50228a47924aeb099021bc46c069109f96
Colin Jansen <cjansen@replicationconsulting.ca>
2022-12-23 02:28:22 -0700

    day22 part 2 --- ugh

--- 2022/day22.py
+++ 2022/day22.py
@@ -3,19 +3,22 @@ import re
 with open('_input/day22.txt') as f:
     lines = f.read().splitlines()
 
-me = None
-trail = {}
 
 def trans(c):
-    if c == '-': return '#'
-    if c == ' ': return '.'
-    if c == '.': return ' '
-    if c == '#': return '*'
-    if c == 'M': return 'M'
+    if c == '-':
+        return '#'
+    if c == ' ':
+        return '.'
+    if c == '.':
+        return ' '
+    if c == '#':
+        return '*'
+    if c == 'M':
+        return 'M'
     return c
 
 
-def draw_grid(grid):
+def draw_grid(grid, trail, me):
     for key in trail:
         grid[key[0]][key[1]] = trail[key]
     grid[me[0]][me[1]] = 'M'
@@ -23,8 +26,19 @@ def draw_grid(grid):
         print(''.join([trans(c) for c in g]))
 
 
-def get_grid(lines):
-    global me
+def draw_grid2(grid, trail, me, warps):
+    for key in trail:
+        grid[key[0]][key[1]] = trail[key]
+    for key in warps:
+        w = warps[key]
+        grid[key[0]][key[1]] = 'w'
+        grid[w[0]][w[1]] = 'o'
+    grid[me[0]][me[1]] = 'M'
+    for g in grid:
+        print(''.join([trans(c) for c in g]))
+
+
+def get_grid(lines, me=None):
     # get max line length
     m = 0
     for i in range(len(lines) - 2):
@@ -36,11 +50,11 @@ def get_grid(lines):
         for x in range(len(lines[y])):
             c = lines[y][x]
             if me == None and c == '.':
-                me = (y + 1, x + 1, '>') # starting position
+                me = (y + 1, x + 1, '>')  # starting position
             if c == ' ':
                 continue
             grid[y + 1][x + 1] = lines[y][x]
-    return grid
+    return grid, me
 
 
 def get_moves(lines):
@@ -52,71 +66,158 @@ def get_moves(lines):
     return moves
 
 
-def turn(d):
-    global me
+def turn(d, me):
     if d == 'R':
-        if me[2] == '>': return (me[0], me[1], 'v')
-        if me[2] == 'v': return (me[0], me[1], '<')
-        if me[2] == '<': return (me[0], me[1], '^')
-        if me[2] == '^': return (me[0], me[1], '>')
+        if me[2] == '>':
+            return (me[0], me[1], 'v')
+        if me[2] == 'v':
+            return (me[0], me[1], '<')
+        if me[2] == '<':
+            return (me[0], me[1], '^')
+        if me[2] == '^':
+            return (me[0], me[1], '>')
     if d == 'L':
-        if me[2] == '<': return (me[0], me[1], 'v')
-        if me[2] == '^': return (me[0], me[1], '<')
-        if me[2] == '>': return (me[0], me[1], '^')
-        if me[2] == 'v': return (me[0], me[1], '>')
+        if me[2] == '<':
+            return (me[0], me[1], 'v')
+        if me[2] == '^':
+            return (me[0], me[1], '<')
+        if me[2] == '>':
+            return (me[0], me[1], '^')
+        if me[2] == 'v':
+            return (me[0], me[1], '>')
     return me
 
 
-def get_next_block():
-    global grid, me
+def get_next_block(grid, me):
     y = me[0]
     x = me[1]
-    if me[2] == '>': x += 1
-    if me[2] == '<': x -= 1 
-    if me[2] == '^': y -= 1
-    if me[2] == 'v': y += 1
+    d = me[2]
+    if me[2] == '>':
+        x += 1
+    if me[2] == '<':
+        x -= 1
+    if me[2] == '^':
+        y -= 1
+    if me[2] == 'v':
+        y += 1
     if (grid[y][x] == ' '):
-        if me[2] == '^': # up
-            while grid[y][x] == ' ': 
+        if me[2] == '^':  # up
+            while grid[y][x] == ' ':
                 y = (y - 1) % len(grid)
-        if me[2] == 'v': # down
-            while grid[y][x] == ' ': 
+        if me[2] == 'v':  # down
+            while grid[y][x] == ' ':
                 y = (y + 1) % len(grid)
-        if me[2] == '>': # right
-            while grid[y][x] == ' ': 
+        if me[2] == '>':  # right
+            while grid[y][x] == ' ':
                 x = (x + 1) % len(grid[y])
-        if me[2] == '<': # left
+        if me[2] == '<':  # left
             while grid[y][x] == ' ':
                 x = (x - 1) % len(grid[y])
-    return (y, x)
+    return (y, x, d)
 
 
-def walk(steps):
-    global me
+def get_next_block2(grid, me, warps):
+    y = me[0]
+    x = me[1]
+    d = me[2]
+    if d == '>':
+        x += 1
+    if d == '<':
+        x -= 1
+    if d == '^':
+        y -= 1
+    if d == 'v':
+        y += 1
+    if (grid[y][x] == ' '):
+        y, x, d = warps[(y, x, d)]
+    return (y, x, d)
+
+
+def walk(steps, grid, me, trail):
     for _ in range(steps):
-        y, x = get_next_block()
-        if grid[y][x] == '.': 
+        y, x, d = get_next_block(grid, me)
+        if grid[y][x] == '.':
             trail[(me[0], me[1])] = me[2]
-            me = (y, x, me[2])
+            me = (y, x, d)
             continue
         if grid[y][x] == '#':
-            return
+            break
+    return me, trail
 
 
-grid = get_grid(lines)
-moves = get_moves(lines)
+def walk2(steps, grid, me, trail, warps):
+    for _ in range(steps):
+        y, x, d = get_next_block2(grid, me, warps)
+        if grid[y][x] == '.':
+            trail[(me[0], me[1])] = me[2]
+            me = (y, x, d)
+            continue
+        if grid[y][x] == '#':
+            break
+    return me, trail
 
-for m in range(0, len(moves), 2):
-    walk(moves[m])
-    me = turn(moves[m+1])
 
-def get_facing():
-    global me
-    if me[2] == '^': return 3
-    if me[2] == '>': return 0
-    if me[2] == 'v': return 1
-    if me[2] == '<': return 2
+def get_facing(me):
+    if me[2] == '^':
+        return 3
+    if me[2] == '>':
+        return 0
+    if me[2] == 'v':
+        return 1
+    if me[2] == '<':
+        return 2
 
-#draw_grid(grid)
-print(f'1000 * {me[0]} + 4 * {me[1]} + {get_facing()}')
-print((me[0] * 1000) + (me[1] * 4) + get_facing())
+
+def create_warps(s):
+    warps = {}
+    for i in range(s):
+        # from F to L and L to F
+        warps[((s*2)-i, s, '<')] = ((s*2)+1, s-i, 'v') # ok
+        warps[((s*2), s-i, '^')] = ((s*2)-i, s+1, '>') # ok
+        # from D to R and R to D
+        warps[(s-i, (s*3)+1, '>')] = ((s*2)+i+1, (s*2), '<') # ok
+        warps[((s*2)+i+1, (s*2)+1, '>')] = (s-i, s*3, '<') # ok
+        # from F to R and R to F
+        warps[(s+1, (s*2)+i+1, 'v')] = (s+i+1, (s*2), '<') # ok
+        warps[(s+i+1, (s*2)+1, '>')] = (s, (s*2)+i+1, '^') # ok
+        # from B to D and D to B
+        warps[((s*3)+i+1, s+1, '>')] = ((s*3), s+i+1, '^') # ok
+        warps[(s*3+1, (s)+i+1, 'v')] = ((s*3)+i+1, s, '<') # ok
+        # from U to L and L to U
+        warps[(s-i, s, '<')] = ((s*2)+i+1, 1, '>') # ok
+        warps[((s*2)+i+1, 0, '<')] = (s-i, s+1, '>') # ok
+        # from B to R and R to B
+        warps[((s*4)+1, s-i, 'v')] = (1, (s*3)-i, 'v') # ok
+        warps[(0, (s*3)-i, '^')] = ((s*4), s-i, '^') # ok
+        # from U to B and B to U
+        warps[(0, (s*2)-i, '^')] = ((s*4)-i, 1, '>') # ok
+        warps[((s*4)-i, 0, '<')] = (1, (s*2)-i, 'v')
+    
+    return warps
+
+
+def part1(lines):
+    trail = {}
+    grid, me = get_grid(lines)
+    moves = get_moves(lines)
+    for m in range(0, len(moves), 2):
+        me, trail = walk(moves[m], grid, me, trail)
+        me = turn(moves[m+1], me)
+    print(f'1000 * {me[0]} + 4 * {me[1]} + {get_facing(me)}')
+    return (me[0] * 1000) + (me[1] * 4) + get_facing(me)
+
+
+def part2(lines):
+    trail = {}
+    warps = create_warps(50)
+    grid, me = get_grid(lines)
+    moves = get_moves(lines)
+    for m in range(0, len(moves), 2):
+        me, trail = walk2(moves[m], grid, me, trail, warps)
+        me = turn(moves[m+1], me)
+    draw_grid2(grid, trail, me, warps)
+    print(f'1000 * {me[0]} + 4 * {me[1]} + {get_facing(me)}')
+    return (me[0] * 1000) + (me[1] * 4) + get_facing(me)
+
+
+print(part2(lines))
