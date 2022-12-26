b901c4184c6994fe2739da868e5aeca6af141a5a
Colin Jansen <cjansen@replicationconsulting.ca>
2022-12-25 23:58:10 -0700

    clean up day 25 a bit

--- 2022/day25.py
+++ 2022/day25.py
@@ -1,63 +1,70 @@
 
 
-
 with open('_input/day25.txt') as f:
     input = f.read().splitlines()
 
-def from_snafu(s: str) -> int:    
+
+def from_snafu(snafu: str) -> int:
+
     SNAFU = {}
     SNAFU['2'] = 2
     SNAFU['1'] = 1
     SNAFU['0'] = 0
     SNAFU['-'] = -1
     SNAFU['='] = -2
-    c = 0
-    for i in range(len(s)):
-        place = len(s) - i - 1
-        v = 5**place * SNAFU[s[i]]
-        c += v
-    return c
 
-def to_snafu(n: int) -> int:
+    number = 0
+    for idx in range(len(snafu)):
+        place = len(snafu) - idx - 1
+        number += 5**place * SNAFU[snafu[idx]]
+    return number
+
+
+def to_snafu(number: int) -> int:
+
+    place = 0
+    snafu = ''
+    snafu_place_values = []
+
     SNAFU = {}
     SNAFU[2] = '2'
     SNAFU[1] = '1'
     SNAFU[0] = '0'
     SNAFU[-1] = '-'
     SNAFU[-2] = '='
-    def get_snafu(n: int, place: int) -> str:
+
+    def get_snafu_at_place(number: int, place: int) -> tuple:
         possibles = {}
         best_index = -2
-        for i in range(-2, 3):
-            val = 5**place * i
-            dif = val - n
-            possibles[i] = (i, val, dif)
+        for idx in range(-2, 3):
+            val = 5**place * idx
+            dif = val - number
+            possibles[idx] = (idx, val, dif)
             if abs(possibles[best_index][2]) > abs(dif):
-                best_index = i
+                best_index = idx
         return possibles[best_index]
 
-    def get_snafu_chars(nums:list[int]) -> str:
-        s = ''
-        for n in nums:
-            s += SNAFU[n]
-        return s
-
-    place = 0
-    s = []
-    while n > 2 * 5**place:
+    # find the 'place' to start from
+    while number > 2 * 5**place:
         place += 1
+    # roll back 'places' until we get to 5**0
     while place >= 0:
-        c = get_snafu(n, place)
+        result = get_snafu_at_place(number, place)
         place -= 1
-        s.append(c[0])
-        n -= c[1]
-    return get_snafu_chars(s)
+        snafu_place_values.append(result[0])
+        number -= result[1]
+    
+    for place_value in snafu_place_values:
+        snafu += SNAFU[place_value]
+        
+    return snafu
+
 
 def part1(lines):
     t = 0
     for line in lines:
         n = from_snafu(line)
-        t +=n
+        t += n
     return to_snafu(t)
 
 
