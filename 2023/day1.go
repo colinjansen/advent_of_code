86c0d399b3d49d713d090a1dc5ff3ef5826ae505
Colin Jansen <cjansen@replicationconsulting.ca>
2023-12-04 21:01:46 -0700

    add day 1 go

--- 2023/day1.go
+++ 2023/day1.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func trans(num string) int {
+	switch num {
+	case "one":
+		return 1
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	default:
+		return 0 // Return 0 if the input string is not a number word
+	}
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	content, err := os.ReadFile("_input/day1.txt")
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	lines := strings.Split(string(content), "\n")
+
+	t := 0
+	re := regexp.MustCompile(`(\d|one|two|three|four|five|six|seven|eight|nine)`)
+	for _, line := range lines {
+		res := re.FindAllString(line, -1)
+		c := trans(res[0]) + trans(res[len(res)-1])
+		t += c
+	}
+	fmt.Println(t)
 }
