b66fe221b31f82949eec9663c6c2fbfb4a26b818
Colin Jansen <cjansen@replicationconsulting.ca>
2023-12-07 00:41:33 -0700

    cleaned up a bit

--- 2023/day7.py
+++ 2023/day7.py
@@ -3,9 +3,11 @@ import functools
 with open("_input/day7.txt", encoding='utf8') as f:
     lines = f.read().splitlines()
 
-C = len(lines)
-H = []
-B = []
+# holders for the hands and bids
+hands = []
+bids = []
+
+# holders for the various kinds of hands
 five = []
 four = []
 full = []
@@ -13,11 +15,6 @@ three = []
 twopair = []
 pair = []
 high = []
-
-for line in lines:
-    l = line.split(' ')
-    H.append(l[0])
-    B.append(l[1])
 
 def card_val(c, with_jokers=False) -> int:
     if c == 'A': return 14
@@ -28,108 +25,104 @@ def card_val(c, with_jokers=False) -> int:
     if c == 'T': return 10
     return int(c)
 
-def step_1_sort(h, b, with_jokers=False):
-    s = {}
-    J = 0
-    _three = []
-    _two = []
+def stage_1_sort(h, b, with_jokers=False):
+    card_sets = {}
+    jokers = 0
+    groups_of_three = []
+    groups_of_two = []
+
     for i in range(len(h)):
         if with_jokers and h[i] == 'J':
-            J += 1
+            jokers += 1
             continue
-        if h[i] in s:
-            s[h[i]] += 1
+        if h[i] in card_sets:
+            card_sets[h[i]] += 1
         else:
-            s[h[i]] = 1
-    for k in s.keys():
-        if s[k] == 5:
+            card_sets[h[i]] = 1
+    for k in card_sets.keys():
+        if card_sets[k] == 5:
             return five.append((h, b))
-        if s[k] == 4:
-            if with_jokers and J == 1:
+        if card_sets[k] == 4:
+            if jokers == 1:
                 return five.append((h, b))
             return four.append((h, b))
-        if s[k] == 3:
-            _three.append(k)
-        if s[k] == 2:
-            _two.append(k)
+        if card_sets[k] == 3:
+            groups_of_three.append(k)
+        if card_sets[k] == 2:
+            groups_of_two.append(k)
 
-    if len(_three) == 1 and len(_two) == 1:
+    if len(groups_of_three) == 1 and len(groups_of_two) == 1:
         return full.append((h, b))
     
-    if len(_three) == 1:
-        if with_jokers:
-            if J == 2:
-                return five.append((h, b))
-            if J == 1:
-                return four.append((h, b))
+    if len(groups_of_three) == 1:
+        if jokers == 2:
+            return five.append((h, b))
+        if jokers == 1:
+            return four.append((h, b))
         return three.append((h, b))
     
-    if len(_two) == 2:
-        if with_jokers:
-            if J == 1:
-                return full.append((h, b))
+    if len(groups_of_two) == 2:
+        if jokers == 1:
+            return full.append((h, b))
         return twopair.append((h, b))
     
-    if len(_two) == 1:
-        if with_jokers:
-            if J == 3:
-                return five.append((h, b))
-            if J == 2:
-                return four.append((h, b))
-            if J == 1:
-                return three.append((h, b))
+    if len(groups_of_two) == 1:
+        if jokers == 3:
+            return five.append((h, b))
+        if jokers == 2:
+            return four.append((h, b))
+        if jokers == 1:
+            return three.append((h, b))
         return pair.append((h, b))
     
-    if with_jokers:
-        if J == 4 or J == 5:
-            return five.append((h, b))
-        if J == 3:
-            return four.append((h, b))
-        if J == 2:
-            return three.append((h, b))
-        if J == 1:
-            return pair.append((h, b))
+    if jokers == 4 or jokers == 5:
+        return five.append((h, b))
+    if jokers == 3:
+        return four.append((h, b))
+    if jokers == 2:
+        return three.append((h, b))
+    if jokers == 1:
+        return pair.append((h, b))
     return high.append((h, b))
 
 def get_value(with_jokers=False):
-    for i in range(len(H)):
-        step_1_sort(H[i], int(B[i]), with_jokers)
 
-    t = 0
+    # make sure our hand lists are empty
+    for group in [high, pair, twopair, three, full, four, five]:
+        group.clear()
+
+    # sort the hands into their groups
+    for i in range(len(hands)):
+        stage_1_sort(hands[i], int(bids[i]), with_jokers)
+
+    #comparison functions for stage 2 sorting
     def step_2_sort(a, b):
         h1 = a[0]
         h2 = b[0]
         for i in range(5):
-            if card_val(h1[i], with_jokers) > card_val(h2[i], with_jokers):
+            v1 = card_val(h1[i], with_jokers)
+            v2 = card_val(h2[i], with_jokers)
+            if v1 > v2:
                 return 1
-            if card_val(h1[i], with_jokers) < card_val(h2[i], with_jokers):
+            if v1 < v2:
                 return -1
         return 0
-
+    
     rank = 0
-    t = 0
-    for e in sorted(high, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(pair, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(twopair, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(three, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(full, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(four, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
-    for e in sorted(five, key=functools.cmp_to_key(step_2_sort)):
-        rank += 1
-        t += rank * e[1]
+    total = 0
+    # for all of the (ordered) kinds of hands
+    for group in [high, pair, twopair, three, full, four, five]:
+        # sort the hands in the group by their value
+        for e in sorted(group, key=functools.cmp_to_key(step_2_sort)):
+            rank += 1
+            total += rank * e[1]
 
-    return t
+    return total 
+
+# parse the input into hands and bids
+for line in lines:
+    l = line.split(' ')
+    hands.append(l[0])
+    bids.append(l[1])
 
 print(f'part 1: {get_value()} part 2: {get_value(True)}')
