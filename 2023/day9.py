ad89b8607b5726927c98e4f31b589ce760fb488d
Colin Jansen <cjansen@replicationconsulting.ca>
2023-12-09 00:26:08 -0700

    making it tighter

--- 2023/day9.py
+++ 2023/day9.py
@@ -7,16 +7,17 @@ def get_to_zero(source):
         entries.append(source)
     return entries
 
-def ex(line):
+def extrapolate(line):
     entries = get_to_zero([int(x) for x in line.split()])
-    accumulator_1 = reduce(lambda a, i: entries[i][-1] + a, [i for i in range(len(entries) - 2, -1, -1)], 0)
-    accumulator_2 = reduce(lambda a, i: entries[i][0] - a, [i for i in range(len(entries) - 2, -1, -1)], 0)
+    indexes = [i for i in range(len(entries) - 2, -1, -1)]
+    accumulator_1 = reduce(lambda a, i: entries[i][-1] + a, indexes, 0)
+    accumulator_2 = reduce(lambda a, i: entries[i][0] - a, indexes, 0)
     return accumulator_1, accumulator_2
 
 with open("_input/day9.txt", encoding='utf8') as f:
     lines = f.read().splitlines()
 
-total_1 = sum([ex(line)[0] for line in lines])
-total_2 = sum([ex(line)[1] for line in lines])
+total_1 = sum([extrapolate(line)[0] for line in lines])
+total_2 = sum([extrapolate(line)[1] for line in lines])
     
 print(f'part 1: {total_1} part 2: {total_2}')
