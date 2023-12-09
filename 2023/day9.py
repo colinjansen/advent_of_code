1e8e8facf2c1e8dfdfa95ba1bca9f65e0aea7590
Colin Jansen <cjansen@replicationconsulting.ca>
2023-12-09 00:20:36 -0700

    making it tighter

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,40 +1,31 @@
-def all_zeros(S):
-    for i in S:
-        if i != 0:
-            return False
-    return True
+from functools import reduce
 
 def get_to_zero(source):
-    entries = []
-    entries.append(source)
-    entry = []
-    current = source
-    while not all_zeros(current):
-        entry = []
-        for i in range(1, len(current)):
-            entry.append(current[i] - current[i-1])
-        entries.append(entry)
-        current = entry
+    entries = [source]
+    while not all(i == 0 for i in source):
+        source = [source[i] - source[i - 1] for i in range(1, len(source))]
+        entries.append(source)
     return entries
+
+def e2x(line):
+    entries = get_to_zero([int(x) for x in line.split()])
+    accumulator_1 = 0
+    accumulator_2 = 0
+    for i in range(len(entries)-2, -1, -1):
+        accumulator_1 = entries[i][-1] + accumulator_1
+        accumulator_2 = entries[i][0] - accumulator_2
+    return (accumulator_1, accumulator_2)
 
 def ex(line):
     entries = get_to_zero([int(x) for x in line.split()])
-    accumulator_1 = 0
-    accumulato_2 = 0
-    for i in range(len(entries)-2, -1, -1):
-        accumulator_1 = entries[i][-1] + accumulator_1
-        accumulato_2 = entries[i][0] - accumulato_2
-    return (accumulator_1, accumulato_2)
-
+    accumulator_1 = reduce(lambda a, i: entries[i][-1] + a, [i for i in range(len(entries) - 2, -1, -1)], 0)
+    accumulator_2 = reduce(lambda a, i: entries[i][0] - a, [i for i in range(len(entries) - 2, -1, -1)], 0)
+    return accumulator_1, accumulator_2
 
 with open("_input/day9.txt", encoding='utf8') as f:
     lines = f.read().splitlines()
 
-total_1 = 0
-total_2 = 0
-for line in lines:
-    extrapolations = ex(line)
-    total_1 += extrapolations[0]
-    total_2 += extrapolations[1]
+total_1 = sum([ex(line)[0] for line in lines])
+total_2 = sum([ex(line)[1] for line in lines])
     
-print(total_1, total_2)
+print(f'part 1: {total_1} part 2: {total_2}')
