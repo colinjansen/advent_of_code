4a47b63b2463ae3fcba9f88f82e5292d872fc6a6
Colin Jansen <cjansen@replicationconsulting.ca>
2023-12-10 01:04:35 -0700

    cleaned up a bit

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,58 +1,80 @@
 with open("_input/day10.txt", encoding="utf8") as f:
     L = f.read().splitlines()
-for i,_ in enumerate(L):
-    L[i] = list(L[i])
 
-
-def find_start(L):
-    for i, r in enumerate(L):
-        for j, c in enumerate(r):
-            if c == "S":
-                return (j, i)
-            
 VISITED = set()
-def move_along_pipe(C):
-    VISITED.add(C)
-    c, r = C
+
+START = next(
+    ((j, i) for i, r in enumerate(L) for j, c in enumerate(r) if c == "S"), None
+)
+
+
+def move_along_pipe(coordinate):
+    VISITED.add(coordinate)
+    c, r = coordinate
     # go up
-    if (c, r - 1) not in VISITED and r > 0 and L[r][c] in ['S','|','L','J'] and L[r - 1][c] in ['|','F','7']:
+    if (
+        (c, r - 1) not in VISITED
+        and r > 0
+        and L[r][c] in ["S", "|", "L", "J"]
+        and L[r - 1][c] in ["|", "F", "7"]
+    ):
         return (c, r - 1)
     # go down
-    if (c, r + 1) not in VISITED and r < len(L) and L[r][c] in ['S','|','F','7'] and L[r + 1][c] in ['|','L','J']:
+    if (
+        (c, r + 1) not in VISITED
+        and r < len(L)
+        and L[r][c] in ["S", "|", "F", "7"]
+        and L[r + 1][c] in ["|", "L", "J"]
+    ):
         return (c, r + 1)
     # go right
-    if (c + 1, r) not in VISITED and c < len(L[0]) and L[r][c] in ['S','-','F','L'] and L[r][c + 1] in ['-','7','J']:
-        return (c+1, r )
+    if (
+        (c + 1, r) not in VISITED
+        and c < len(L[0])
+        and L[r][c] in ["S", "-", "F", "L"]
+        and L[r][c + 1] in ["-", "7", "J"]
+    ):
+        return (c + 1, r)
     # go left
-    if (c - 1, r) not in VISITED and c > 0 and L[r][c] in ['S','-','J','7'] and L[r][c - 1] in ['-','F','L']:
-        return (c-1, r)
+    if (
+        (c - 1, r) not in VISITED
+        and c > 0
+        and L[r][c] in ["S", "-", "J", "7"]
+        and L[r][c - 1] in ["-", "F", "L"]
+    ):
+        return (c - 1, r)
     return False
 
-S = find_start(L)
-v = S
-while v is not False:
-    v = move_along_pipe(v)
-part_1 = len(VISITED)//2
-print(part_1)
 
-def count(r, c):
-    t = 0
+def count_intersections_with_pipe(r, c):
+    intersections = 0
     while r > 0 and c > 0:
+        # moving up and left
         r -= 1
         c -= 1
         if (c, r) in VISITED:
-            if L[r][c] in ['L','7']:
-                t += 2
+            # these corners count as two line intersections
+            if L[r][c] in ["L", "7"]:
+                intersections += 2
             else:
-                t += 1
-    return t
+                intersections += 1
+    return intersections
 
-move_along_pipe = 0
-for r, R in enumerate(L):
-    for c, C in enumerate(R):
-        if (c, r) in VISITED:
-            continue
-        if 1 == count(r, c) % 2:
-            move_along_pipe += 1
 
-print(move_along_pipe)
+# trace the pipe
+v = START
+while v is not False:
+    v = move_along_pipe(v)
+
+# half the length of the pipe
+part_1 = len(VISITED) // 2
+
+# count the contained blocks
+part_2 = sum(
+    1
+    for r, R in enumerate(L)
+    for c, C in enumerate(R)
+    if (c, r) not in VISITED and count_intersections_with_pipe(r, c) % 2 == 1
+)
+
+print(f"part 1: {part_1} part 2: {part_2}")
