14393d774b133bd82f66938f68c2c7be8a0717c8
Colin Jansen <cjansen@replicationconsulting.ca>
2024-12-05 21:48:50 -0700

    2019 day 5 ???

--- 2019/python/day5.py
+++ 2019/python/day5.py
@@ -2,40 +2,48 @@ with open('2019/_input/day5.txt') as fp:
     CODES = fp.readline().split(',')
     CODES = [int(c) for c in CODES]
 
-
 def get_opcode(code):
-    return code[-2:]
+    return int(str(code)[-2:])
 
-def get_param(code, n):
+def get_mode(code, n):
+    code = str(code)
     if n > len(code) - 2:
         return 0
-    return code[-(n+2)]
+    return int(code[-(n+2)])
 
-def go(in1, in2, CODES):
+def get_param(code, n, p, CODES):
+    mode = get_mode(code, n)
+    if mode == 0:
+        return CODES[CODES[p+n]]
+    else:
+        return CODES[p+n]
+    
+def set_param(val, p, CODES):
+    CODES[CODES[p]] = val
+    
+def go(CODES):
     P = 0
-    CODES[1] = in1
-    CODES[2] = in2
-
     while CODES[P] != 99:
+        code = CODES[P]
         op = get_opcode(CODES[P])
-        p1 = CODES[P+1]
-        p2 = CODES[P+2]
-        p3 = CODES[P+3]
-        if CODES[P] == 1:
-            CODES[p3] = CODES[p1] + CODES[p2]
+        if op == 1:
+            v1 = get_param(code, 1, P, CODES) 
+            v2 = get_param(code, 2, P, CODES)
+            set_param(v1+v2, P+3, CODES)
             P += 4
-        if CODES[P] == 2:
-            CODES[p3] = CODES[p1] * CODES[p2]
+        if op == 2:
+            v1 = get_param(code, 1, P, CODES) 
+            v2 = get_param(code, 2, P, CODES)
+            set_param(v1*v2, P+3, CODES)
             P += 4
-        if CODES[P] == 3:
-            CODES[p3] = CODES[p1] * CODES[p2]
+        if op == 3:
+            CODES[CODES[P+1]] = 1 
             P += 2
-        if CODES[P] == 4:
-            CODES[p3] = CODES[p1] * CODES[p2]
+        if op == 4:
+            print(CODES[CODES[P+1]])
             P += 2
-    return CODES[0]
+    return 
 
-# print(go(12, 2, CODES[:]))
 
 # for i in range(100):
 #     for j in range(100):
