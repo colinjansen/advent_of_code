aa11dc628351976f4d6976f1eaeb1fa33d9777fd
Colin Jansen <cjansen@Mac.localdomain>
2024-12-10 23:58:29 -0700

    2024 day 11

--- 2024/python/day11.py
+++ 2024/python/day11.py
@@ -5,23 +5,24 @@ with open('2024/_input/day11.txt') as f:
     stones = [int(s) for s in f.read().strip().split()]
 
 @cache
-def blink(stone:int, n:int) -> int:
-    if n == 0: 
+def blink(stone:int, n:int, target:int) -> int:
+    if n == target: 
         return 1
     
+    n += 1
     if stone == 0: 
-        return blink(1, n - 1)
+        return blink(1, n, target)
     
     stone_as_string = str(stone)
     length_of_stone = len(stone_as_string)
 
     if length_of_stone % 2:
-        return blink(stone * 2024, n - 1)
+        return blink(stone * 2024, n, target)
     
     left = stone_as_string[:length_of_stone // 2]
     right = stone_as_string[length_of_stone // 2:]
-    return blink(int(left), n - 1) + blink(int(right), n - 1)
+    return blink(int(left), n, target) + blink(int(right), n, target)
     
 
-print('part 1', sum(blink(c, 25) for c in stones))
-print('part 2', sum(blink(c, 75) for c in stones))
+print('part 1', sum(blink(c, 0, 25) for c in stones))
+print('part 2', sum(blink(c, 0, 75) for c in stones))
