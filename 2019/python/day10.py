a7798a500a15da5d31b9230f141d185cd65d917a
Colin Jansen <cjansen@lawdepot.com>
2024-12-12 14:03:00 -0700

    2019 day 10

--- 2019/python/day10.py
+++ 2019/python/day10.py
@@ -1,8 +1,72 @@
-MAP = []
-with open('2019/_input/day10.txt') as f:
-    for line in f.readlines():
-        MAP.append(line.strip())
-def get_starts():
-    return [(x, y) for x, r in enumerate(MAP) for y, c in enumerate(r )if c == '#']
+from collections import defaultdict
+import math
 
-print(get_starts())
+map = []
+map2 = []
+with open(f'2019/_input/day10.txt') as f:
+    for line in f.readlines():
+        map.append(line.strip())
+        map2.append(list(line.strip()))
+
+def get_points(map, f):
+    return [(x, y) for x, r in enumerate(map) for y, c in enumerate(r) if f(c)]
+
+
+def slope_to_degrees(rise, run, angle_offset_from_east=90):
+    return (math.degrees(math.atan2(rise, run)) + angle_offset_from_east) % 360
+    
+    
+def get_asteroid_info(asteroid, asteroids):
+    info = []
+    
+    for a in asteroids:
+        if a == asteroid:
+            continue
+        dx, dy = a[0] - asteroid[0], a[1] - asteroid[1]
+        gcd = math.gcd(dx, dy)
+        info.append({
+            'position': a, 
+            'slope': (dx / gcd, dy / gcd),
+            'angle': slope_to_degrees(dx, dy),
+            'distance': abs(dx + dy)
+        })
+    return info
+
+def dict_from_list(list_of_items:list, key:str):
+    d = defaultdict(list)
+    for item in list_of_items:
+        d[item[key]].append(item)
+    return d
+
+asteroids = get_points(map, lambda c: c == '#')
+
+part1 = (0, None)
+for asteroid in asteroids:
+    info = get_asteroid_info(asteroid, asteroids)
+    slopes = dict_from_list(info, 'slope')
+    if len(slopes) > part1[0]:
+        part1 = (len(slopes), asteroid, info)
+
+print('part 1', part1[0], part1[1])
+
+A = part1[2]
+angles = dict_from_list(A, 'angle')
+angles = dict(sorted(angles.items()))
+
+# sort asteroids by distance
+for angle, asteroids in angles.items():
+    angles[angle] = sorted(asteroids, key=lambda a: a['distance'])
+
+i = 0
+t = 200
+while i < 200:
+    if 0 == sum(len(asteroids) for asteroids in angles.values()):
+        print('all out of asteroids')
+        break
+    for angle, asteroids in angles.items():
+        if asteroids:
+            a = asteroids.pop(0)
+            i += 1
+            if i == t:
+                print('part 2', a['position'][1]*100+a['position'][0], a)
+                break
--- 2021/.net/src/obj/Debug/net5.0/Advent.AssemblyInfo.cs
+++ 2021/.net/src/obj/Debug/net5.0/Advent.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d71027822075f2f434a293a3fb62254dcaea533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a58445ca840eda09ef16abd170a6a1e2567d882")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
