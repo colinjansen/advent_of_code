66383d336e48fc0a71d7d05c0f1af6b9096225d1
Colin Jansen <cjansen@Mac.localdomain>
2024-12-13 21:44:07 -0700

    2024 day 11

--- 2024/python/day11.py
+++ 2024/python/day11.py
@@ -5,6 +5,20 @@ with open('2024/_input/day11.txt') as f:
     stones = [int(s) for s in f.read().strip().split()]
 
 @cache
+def calc(stone):
+    if stone == 0:
+        return [1]
+    
+    s = str(stone)
+    if len(s) % 2:
+        return [stone * 2024]
+    
+    l = s[:len(s) // 2]
+    r = s[len(s) // 2:]
+    return [int(l), int(r)]
+
+
+@cache
 def blink(stone:int, n:int, target:int) -> int:
     if n == target: 
         return 1
@@ -22,7 +36,17 @@ def blink(stone:int, n:int, target:int) -> int:
     left = stone_as_string[:length_of_stone // 2]
     right = stone_as_string[length_of_stone // 2:]
     return blink(int(left), n, target) + blink(int(right), n, target)
-    
 
-print('part 1', sum(blink(c, 0, 25) for c in stones))
-print('part 2', sum(blink(c, 0, 75) for c in stones))
+
+V = {}
+for wer in range(75):
+    new_stones = []
+    for stone in stones:
+        if stone not in V:
+            V[stone] = calc(stone)
+        new_stones.extend(V[stone])
+    stones = new_stones
+print(len(stones ))
+
+#print('part 1', sum(blink(c, 0, 25) for c in stones))
+#print('part 2', sum(blink(c, 0, 75) for c in stones))
