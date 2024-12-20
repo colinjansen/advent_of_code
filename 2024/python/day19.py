ca01bb05541f53a82b3c96892384a02ee94fd702
Colin Jansen <cjansen@replicationconsulting.ca>
2024-12-20 12:53:29 -0700

    2024 day 19 part 2

--- 2024/python/day19.py
+++ 2024/python/day19.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+from functools import cache
+
+
 def get_parts():
     with open('2024/_input/day19.txt') as f:
         parts = f.readline()
@@ -8,7 +12,7 @@ def get_parts():
         quilts = [q.strip() for q in quilts]
     return parts, quilts
 
-def can_make(quilt, parts, i=0, memo=None):
+def can_make(quilt, i=0, memo=None):
     """
     Returns True if the quilt can be made from the parts, False otherwise.
 
@@ -29,7 +33,7 @@ def can_make(quilt, parts, i=0, memo=None):
             # and it matches the quilt
             quilt[i:i+len(part)] == part and  
             # and we can complete the quilt
-            can_make(quilt, parts, i + len(part), memo)):
+            can_make(quilt, i + len(part), memo)):
             # remember that from this point, with the previous parts
             # we can make the quilt
             memo[i] = True
@@ -37,10 +41,23 @@ def can_make(quilt, parts, i=0, memo=None):
     memo[i] = False
     return False
 
+def can_make_all(quilt, i=0, memo={}):
+    if i in memo:
+        return memo[i]
+    if i == len(quilt):
+        return 1
+    count = 0
+    for part in parts:
+        new_length = i + len(part)
+        if new_length <= len(quilt) and quilt[i:new_length] == part:
+            count += can_make_all(quilt, new_length, memo=memo)
+    memo[i] = count
+    return count
+
 parts, quilts = get_parts()
 
-part1 = 0
-for i, quilt in enumerate(quilts):
-    part1 += can_make(quilt, parts)
+part1 = sum(can_make(quilt) for quilt in quilts)
+part2 = sum(can_make_all(quilt) for quilt in quilts)
 
-print(part1)
+print('part 1:', part1)
+print('part 2:', part2)
